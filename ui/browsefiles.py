a184b8fdcd0365666f1d96170630bc00b6523aaf
Ein <einnuian@gmail.com>
2025-05-01 23:52:41 -0400

    Remove OS detection

--- ui/browsefiles.py
+++ ui/browsefiles.py
@@ -6,21 +6,8 @@ class Browser:
     def __init__(self):
         self.root = tk.Tk()
         self.root.withdraw()
-
-        # Detect the OS the program is running on
-        os_type = Helper.detect_os()
-        initial_dir = ""
-
-        # Assuming that the user is running WSL and storing the file on the Window's file system
-        if os_type == "Linux":
-            username = input("Enter your Windows' username: ")
-            initial_dir = "/mnt/c/Users/" + username
-            print(initial_dir)
-        else: pass
-
-
+        
         self.file_path = filedialog.askopenfilename(
-                initialdir=initial_dir, 
                 title='Select A File',
                 filetypes=[('csv files', "*.csv")] # '*.csv' = any_name.csv
                 )
